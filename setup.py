15ee080cc2ccd7aefe6a7085b921aa4b7eac9d51
VaishaliGoel11 <vaishaligoel11@outlook.com>
2026-02-18 22:58:25 +0530

    project basic structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Saurabh Goel",
+    author_email="saurabhkumar0009@gmail.com",
+    packages=find_packages()
+)
